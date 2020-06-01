a9a9ca019cacc81ccdc9012ee4a9af8aae9e470c
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-06-01 11:10:46 +0300

    create a class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Confirm from './Confirm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Confirm
+          title="React and TypeScript"
+          content="Are you sure you want to learn React and TypeScript?"
+          cancelCaption="No way"
+          okCaption="Yes please!"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
-
-export const Header: React.FC = () => {
-  const name: string = 'Fred';
-  return <div>Hello</div>;
-};
-
-function sayHello(name: string) {
-  console.log(`Hello ${name}`);
-}
-
-sayHello(9);

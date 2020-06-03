4d1854cab229d7407a7c6bf568f69930272447c4
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-06-03 12:03:54 +0300

    add functional components

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Confirm from './Confirm';
+import ConfirmF from './ConfirmF';
 
 interface IState {
   confirmOpen: boolean;
   confirmMessage: string;
+  confirmVisible: boolean;
+  countDown: number;
 }
 
 class App extends React.Component<{}, IState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      confirmOpen: true,
+      confirmOpen: false,
       confirmMessage: 'Please hit the confirm button',
+      confirmVisible: true,
+      countDown: 10,
     };
   }
+  private timer: number = 0;
 
   private handleConfirmClick = () => {
     this.setState({
       confirmOpen: true,
       confirmMessage: 'Cool, carry on reading!',
     });
+    clearInterval(this.timer);
   };
 
   private handleCancelConfirmClick = () => {
@@ -29,11 +36,45 @@ class App extends React.Component<{}, IState> {
       confirmOpen: false,
       confirmMessage: "Take a break, I'm sure you will later ...",
     });
+    clearInterval(this.timer);
   };
 
   private handleOkConfirmClick = () => {
     this.setState({ confirmOpen: false });
+    clearInterval(this.timer);
   };
+
+  private handleTimerTick() {
+    this.setState(
+      {
+        confirmMessage: `Please hit the confirm button ${this.state.countDown} secs to go`,
+        countDown: this.state.countDown - 1,
+      },
+      () => {
+        if (this.state.countDown <= 0) {
+          clearInterval(this.timer);
+          this.setState({
+            confirmMessage: 'Too late to confirm!',
+            confirmVisible: false,
+          });
+        }
+      }
+    );
+  }
+
+  public static getDerivedStateFromProps(props: {}, state: IState) {
+    console.log('getDerivedStateFromProps', props, state);
+    return null;
+  }
+
+  public componentDidMount() {
+    this.timer = window.setInterval(() => this.handleTimerTick(), 1000);
+  }
+
+  public componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
     return (
       <div className="App">
@@ -52,8 +93,10 @@ class App extends React.Component<{}, IState> {
           </a>
         </header>
         <p>{this.state.confirmMessage}</p>
-        <button onClick={this.handleConfirmClick}>Confirm</button>
-        <Confirm
+        {this.state.confirmVisible && (
+          <button onClick={this.handleConfirmClick}>Confirm</button>
+        )}
+        <ConfirmF
           title="React and TypeScript"
           content="Are you sure you want to learn React and TypeScript?"
           cancelCaption="No way"
---
+++ src/ConfirmF.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import './Confirm.css';
+
+interface IProps {
+  open: boolean;
+  title: string;
+  content: string;
+  cancelCaption?: string;
+  okCaption?: string;
+  onOkClick: () => void;
+  onCancelClick: () => void;
+}
+
+const ConfirmF: React.SFC<IProps> = (props) => {
+  const [cancelClickCount, setCancelClickCount] = React.useState(0);
+
+  const handleCancelClick = () => {
+    const newCount = cancelClickCount + 1;
+    setCancelClickCount(newCount);
+    if (newCount >= 2) {
+      props.onCancelClick();
+    }
+  };
+
+  const handleOkClick = () => {
+    props.onOkClick();
+  };
+
+  React.useEffect(() => {
+    console.log('Confirm first rendering');
+  }, []);
+
+  return (
+    <div
+      className={
+        props.open ? 'confirm-wrapper confirm-visible' : 'confirm-wrapper'
+      }
+    >
+      <div className="confirm-container">
+        <div className="confirm-title-container">
+          <span>{props.title}</span>
+        </div>
+        <div className="confirm-content-container">
+          <p>{props.content}</p>
+        </div>
+        <div className="confirm-buttons-container">
+          <button className="confirm-cancel" onClick={handleCancelClick}>
+            {cancelClickCount === 0 ? props.cancelCaption : 'Really?'}
+          </button>
+          <button className="confirm-ok" onClick={handleOkClick}>
+            {props.okCaption}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ConfirmF.defaultProps = {
+  cancelCaption: 'Cancel',
+  okCaption: 'Okay',
+};
+
+export default ConfirmF;
